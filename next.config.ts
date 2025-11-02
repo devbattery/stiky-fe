6ab81bd57039a3dee9db6a2045f2e8b011b6e7c6
devbattery <devbattery@outlook.com>
2025-11-02 13:51:02 +0900

    Feat: Next Config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "2mb",
+        },
+    },
 };
 
 export default nextConfig;
